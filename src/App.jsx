08404e50c4d2342cc049b430b95d521533fcd9b9
Everton <everton.walker@hotmail.com>
2021-04-13 15:48:18 -0300

    Como criar vários componentes filhos dentro de um component pai e receber propriedades do pai passando para o filho

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragment";
 import NumeroAleatorio from "./components/basicos/NumeroAleatorio";
 import Card from "./components/basicos/layout/Card";
+import Familia from "./components/basicos/Familia";
 
 import "./App.css";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
 
 // Quando se tem o default, vc pode retornar uma função anônima, caso não, precisa exportar uma função nomeada
 const App = () => (
@@ -14,13 +16,20 @@ const App = () => (
     <h1>Fundamentos React</h1>
 
     <div className="Cards">
-      <Card titulo="#04 Desafio número aleatório">
+      <Card titulo="#05 Componente pai e filho" color="#1F91">
+        <Familia lastName="Son">
+          <FamiliaMembro name="Walker"></FamiliaMembro>
+          <FamiliaMembro name="Everton"></FamiliaMembro>
+          <FamiliaMembro name="Pro"></FamiliaMembro>
+        </Familia>
+      </Card>
+      <Card titulo="#04 Desafio número aleatório" color="#080">
         <NumeroAleatorio minimo="10" maximo="100"></NumeroAleatorio>
       </Card>
-      <Card titulo="#03 Fragmento">
+      <Card titulo="#03 Fragmento" color="#008">
         <Fragmento></Fragmento>
       </Card>
-      <Card titulo="#02 Com parâmetro">
+      <Card titulo="#02 Com parâmetro" color="#F46900">
         <ComParametro titulo="Segundo componente" subtitulo="Muito legal" />
       </Card>
       <Card titulo="#01 Primeiro componente">
--- src/components/basicos/layout/Card.jsx
+++ src/components/basicos/layout/Card.jsx
@@ -4,8 +4,13 @@ import "./Card.css";
 const Card = (props) => {
   const { titulo, children } = props;
 
+  const cardStyle = {
+    backgroundColor: props.color || "#F00",
+    borderColor: props.color || "#F00",
+  };
+
   return (
-    <div className="Card">
+    <div className="Card" style={cardStyle}>
       <div className="Title">{titulo}</div>
       <div className="Content">{children}</div>
     </div>
